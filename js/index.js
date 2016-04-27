bdff6e83ba7d95db5de08c46eb3a72880e0bd134
ts1027 <443633568@qq.com>
2016-04-28 00:58:27 +0800

    轮播的添加

--- js/index.js
+++ js/index.js
@@ -5,4 +5,18 @@ $(function(){
         box.slideToggle(300);
     })
 
+    //    banner
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false,
+        spaceBetween: 0,
+        loop: true
+    });
+
 })
